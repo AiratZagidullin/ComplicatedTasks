5afbd1295796914a939ef6ea6d9545e54b99f896
Airat Zagidullin <zagbomar@gmail.com>
2022-05-30 18:21:02 +0500

    Выполнил усложненное задание 03

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-let num = 266219;
-let needNum = 1;
+'use strict';
 
-while (num >= 1) {
-    needNum *= (num % 10);
-    num = Math.trunc(num /= 10);
+let lang = 'en';
+if (lang == 'ru') {
+    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+} else if (lang == 'en') {
+    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
 }
 
-needNum = needNum ** 3;
-needNum = needNum.toString();
+switch (lang) {
+    case 'ru':
+        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
+        break
+    case 'en':
+        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+        break
+}
 
-console.log(needNum.substr(0, 2));
+let langArray = {
+    'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+};
+console.log(langArray[lang]);
+
+let namePerson = "Артем";
+let result = namePerson === "Артем" ? "Директор" : namePerson === "Александр" ? "Преподаватель" : "Студент";
+console.log(result);
