163f42cab931c66bc4806d9b4d3969757456e3f6
Airat Zagidullin <zagbomar@gmail.com>
2022-06-07 22:27:17 +0500

    Сделал усложненное задание 09

--- script.js
+++ script.js
@@ -1,45 +1,81 @@
 'use strict';
 
-let week = ['Понедельник', 'Суббота', 'Среда', 'Четверг', 'Воскресенье', 'Пятница', 'Вторник'];
-const date = new Date();
-const today = date.getDay();
+const aClock = function () {
+    let now = new Date();
+    let dayOfWeek = now.getDay();
+    let day = now.getDate();
+    let nowMonth = now.getMonth();
+    let year = now.getFullYear();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let seconds = now.getSeconds();
 
-const weekend = function () {
-    for (let i = 0; i < 7; i++) {
-        if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
-            week[i] = `<i>${week[i]}</i>`
+    const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    const month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+    const getHours = function () {
+        if (hour == 1 || hour == 21) {
+            return hour = hour + ' час '
+        } else if (hour == 2 || hour == 3 || hour == 4 || hour == 22 || hour == 23 || hour == 24) {
+            return hour = hour + ' часа '
+        } else
+            return hour = hour + ' часов '
+    }
+
+    const getMinutes = function () {
+        if ((minute % 10 == 1) && (minute != 11)) {
+            return minute = minute + ' минута '
+        } else if (((minute % 10 == 2) && (minute != 12)) || ((minute % 10 == 3) && (minute != 13)) || ((minute % 10 == 4) && (minute != 14))) {
+            return minute = minute + ' минуты '
+        } else
+            return minute = minute + ' минут '
+    }
+
+    const getSeconds = function () {
+        if ((seconds % 10 == 1) && (seconds != 11)) {
+            return seconds = seconds + ' секунда '
+        } else if (((seconds % 10 == 2) && (seconds != 12)) || ((seconds % 10 == 3) && (seconds != 13)) || ((seconds % 10 == 4) && (seconds != 14))) {
+            return seconds = seconds + ' секунды '
+        } else
+            return seconds = seconds + ' секунд '
+    }
+
+    getHours();
+    getMinutes();
+    getSeconds();
+
+    return 'Сегодня ' + week[dayOfWeek] + ', ' + day + ' ' + month[nowMonth] + ' ' + year + ' года, ' + hour + minute + seconds;
+}
+
+const bClock = function () {
+    let now = new Date();
+
+    const zero = function (value) {
+        if (value < 10) {
+            value = '0' + value;
         }
+        return value;
     }
+
+    const nowDate = function () {
+        let day = zero(now.getDate());
+        let nowMonth = zero(now.getMonth() + 1);
+        let year = zero(now.getFullYear());
+        let hour = zero(now.getHours());
+        let minute = zero(now.getMinutes());
+        let seconds = zero(now.getSeconds());
+        return day + "." + nowMonth + "." + year + " - " + hour + ":" + minute + ":" + seconds;
+    }
+
+    return nowDate();
 }
 
-const weekDay = function () {
-    let i;
-    switch (true) {
-        case today === 1:
-            i = week.indexOf('Понедельник');
-            return week[i] = `<b>${week[i]}</b>`
-        case today === 2:
-            i = week.indexOf('Вторник');
-            return week[i] = `<b>${week[i]}</b>`
-        case today === 3:
-            i = week.indexOf('Среда');
-            return week[i] = `<b>${week[i]}</b>`
-        case today === 4:
-            i = week.indexOf('Четверг');
-            return week[i] = `<b>${week[i]}</b>`
-        case today === 5:
-            i = week.indexOf('Пятница');
-            return week[i] = `<b>${week[i]}</b>`
-        case today === 6:
-            i = week.indexOf('Суббота');
-            return week[i] = `<i><b>${week[i]}</b></i>`
-        case today === 0:
-            i = week.indexOf('Воскресенье');
-            return week[i] = `<i><b>${week[i]}</b></i>`
-    }
-}
+setInterval(function () {
+    document.getElementById('current_time_block1').innerHTML = aClock();
+}, 1000)
 
-weekDay();
-weekend();
+setInterval(function () {
+    document.getElementById('current_time_block2').innerHTML = bClock();
+}, 1000)
 
-document.write(week.join("<br/>"));
+
